d07878947442da433e12002aea3e850328ba0e72
diekus <diekus@gmail.com>
2018-05-25 12:48:01 +0100

    simplifies devDet
    -doesnt depend on AFrame devices funcitons anymore.
    -tries to depend solely on WebVR 1.1
    -will be used with WMR headset on Edge
    -needs more proofing to identify controllers not in movement in Firefox with an Oculus RIft

--- scripts/devDet.js
+++ scripts/devDet.js
@@ -1,47 +1,41 @@
 let detectedDevice;
 
-//device detection
-function deviceDetectionAF(){
-    navigator.getVRDisplays().then(function (displays) {
-        if(AFDevice.isGearVR()){
-            detectedDevice = device.type.GEARVR;
-        }
-        else if(AFDevice.isMobile()){
-            detectedDevice = device.type.MOBILE;
-        }
-        else if (displays.length > 0){ //trys to match high end headsets
-             switch (displays[0].displayName) {
-                case 'Oculus VR HMD':
-                    detectedDevice = device.type.RIFT;
-                    break;
-                case 'HTC Vive MV':
-                    detectedDevice = device.type.VIVE;
-                    break;          
-                default: //undetected
-                    console.log('undetected device name: ' + displays[0].displayName);
-                    break;
-            }
-        }
-        else {detectedDevice = device.type.UNKNOWN;}
-        displayDevice = displays[0];
-    });
-};
-
 let deviceDetection = function(){
     if(navigator.getVRDisplays) {
         console.log('WebVR 1.1 supported');
         navigator.getVRDisplays().then(function(displays) {
-          if(displays.length > 0) {
-            detectedDevice = displays[0];
-          }
-          else{
-              console.log('no device detected');
-          }
+            if(displays.length > 0) {
+                detectedDevice = displays[0];
+                console.log(detectedDevice.displayName);
+                window.addEventListener("gamepadconnected", function(e) {
+                    console.log(`controller ${e.gamepad.index} (${e.gamepad.id}) connected`);
+                    if(e.gamepad.pose && e.gamepad.pose.hasOrientation && e.gamepad.pose.hasPosition){
+                        do_devDet_6DoF(e.gamepad.id);
+                    }
+                    else if(e.gamepad.pose && e.gamepad.pose.hasOrientation){
+                        do_devDet_3DoF(e.gamepad.id);
+                    }
+                    else {
+                        do_devDet_0DoF();
+                    }
+                });
+                window.addEventListener("gamepaddisconnected", function(e) {
+                    console.log(`controller ${e.gamepad.index} (${e.gamepad.id}) disconnected`);
+                    do_devDet_disconnect(e.gamepad.id);
+                });
+            }
+            else{
+                console.log('no device detected'); // adds cursor
+                do_devDet_0DoF();
+            }
         });
       }
       else{
-          console.log('No WebVR support');
+          console.log('No WebVR support'); // adds cursor
+          do_devDet_0DoF();
       }
 };
 
-deviceDetection();
+document.addEventListener('DOMContentLoaded', function() {
+    deviceDetection();
+});
