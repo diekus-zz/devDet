cf7aed9ecc18cd51d97cf1d79fc3da3e0af8d070
diekus <diekus@gmail.com>
2018-05-28 22:14:17 +0100

    separates vr and controller detection

--- scripts/devDet.js
+++ scripts/devDet.js
@@ -1,31 +1,15 @@
 let detectedDevice;
 
-let deviceDetection = function(){
+let vrDetection = function(){
     if(navigator.getVRDisplays) {
         console.log('WebVR 1.1 supported');
         navigator.getVRDisplays().then(function(displays) {
-            if(displays.length > 0) {
+            if(displays.length > 0) { // if there are VR devices present
                 detectedDevice = displays[0];
-                console.log(detectedDevice.displayName);
-                window.addEventListener("gamepadconnected", function(e) {
-                    console.log(`controller ${e.gamepad.index} (${e.gamepad.id}) connected`);
-                    if(e.gamepad.pose && e.gamepad.pose.hasOrientation && e.gamepad.pose.hasPosition){
-                        do_devDet_6DoF(e.gamepad.id, e.gamepad.hand);
-                    }
-                    else if(e.gamepad.pose && e.gamepad.pose.hasOrientation){
-                        do_devDet_3DoF(e.gamepad.id, e.gamepad.hand);
-                    }
-                    else {
-                        do_devDet_0DoF();
-                    }
-                });
-                window.addEventListener("gamepaddisconnected", function(e) {
-                    console.log(`controller ${e.gamepad.index} (${e.gamepad.id}) disconnected`);
-                    do_devDet_disconnect(e.gamepad.id);
-                });
+                console.log(detectedDevice.displayName); 
             }
-            else{
-                console.log('no device detected'); // adds cursor
+            else{ // if there are no vr devices present adds cursor
+                console.log('no device detected'); 
                 do_devDet_0DoF();
             }
         });
@@ -37,5 +21,27 @@ let deviceDetection = function(){
 };
 
 document.addEventListener('DOMContentLoaded', function() {
-    deviceDetection();
+    vrDetection();
+    controller_detection();
 });
+
+let controller_detection = function(){
+    if(navigator.getGamepads != null){
+        window.addEventListener("gamepadconnected", function(e) {
+            console.log(`controller ${e.gamepad.index} (${e.gamepad.id}) connected`);
+            if(e.gamepad.pose && e.gamepad.pose.hasOrientation && e.gamepad.pose.hasPosition){
+                do_devDet_6DoF(e.gamepad.index, e.gamepad.hand);
+            }
+            else if(e.gamepad.pose && e.gamepad.pose.hasOrientation){
+                do_devDet_3DoF(e.gamepad.index, e.gamepad.hand);
+            }
+            else {
+                do_devDet_0DoF(e.gamepad.index);
+            }
+        });
+        window.addEventListener("gamepaddisconnected", function(e) {
+            console.log(`controller ${e.gamepad.index} (${e.gamepad.id}) disconnected`);
+            do_devDet_disconnect(e.gamepad.index);
+        });
+    }
+};
